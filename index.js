1ee5bb218eacebb050ee868c36c3638689ab7d2a
my-rh-r1020 <myrh.comp@gmail.com>
2020-10-21 14:15:48 +0700

    Merubah var nama

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-let nama = prompt("Enter Your Name");
+// File ini untuk testing GIT Remote from local to GitHub
+let nama = prompt("Enter Your Name Please");
 let nim = parseInt(prompt("Enter Your NIM"));
 
 console.log(nama, nim);
