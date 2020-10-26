27d755d5a9a698b82ebb8ff025d1af884929311e
my-rh-r1020 <myrh.comp@gmail.com>
2020-10-26 11:15:20 +0700

    Tambah var alamat pada index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // File ini untuk testing GIT Remote from local to GitHub
 let nama = prompt("Enter Your Name Please");
 let nim = parseInt(prompt("Enter Your NIM"));
+let alamat = prompt("Enter Your Address");
 
 console.log(nama, nim);
+console.log(alamat);
