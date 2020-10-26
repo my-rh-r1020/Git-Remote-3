25a127a2e3ab1595a12be73a33b01762df9007c9
my-rh-r1020 <myrh.comp@gmail.com>
2020-10-26 11:27:02 +0700

    Merubah var alamat menjadi var nilai pada index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // File ini untuk testing GIT Remote from local to GitHub
 let nama = prompt("Enter Your Name Please");
 let nim = parseInt(prompt("Enter Your NIM"));
-let alamat = prompt("Enter Your Address");
+let nilai = parseInt(prompt("Enter Your Grade Previous Semester"));
 
 console.log(nama, nim);
-console.log(alamat);
+console.log(nilai);
