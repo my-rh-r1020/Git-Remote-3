834c0bd09f144610cd2ccbb897bd71a3bade63cb
my-rh-r1020 <myrh.comp@gmail.com>
2020-10-26 11:53:05 +0700

    Menghapus var nim pada index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // File ini untuk testing GIT Remote from local to GitHub
 let nama = prompt("Enter Your Name Please");
-let nim = parseInt(prompt("Enter Your NIM"));
 let nilai = parseInt(prompt("Enter Your Grade Previous Semester"));
 
-console.log(nama, nim);
+console.log(nama);
 console.log(nilai);
